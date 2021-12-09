b359a0cccfa57fe32a4523d93e83e242a0d4bd31
Chris Morris <cmorris@Chriss-iMac-2.local>
2021-12-09 15:43:10 +0000

    Initial setup of client

--- tracker-server/routes/trackRoutes.js
+++ tracker-server/routes/trackRoutes.js
@@ -9,8 +9,26 @@ const router = express.Router();
 router.use(requireAuth);
 
 router.get('/tracks', async (req, res) => {
-    const tracks = await Track.find({userId: req.user._id});
+    const tracks = await Track.find({ userId: req.user._id });
 
     res.status(200).send(tracks);
 });
+
+router.post('/tracks', async (req, res) => {
+    const { name, locations } = req.body;
+
+    if (!name || !locations) {
+        return res.status(422).send({ error: 'You must provide a name and locations' });
+    };
+
+    try {
+        const track = new Track({ name, locations, userId: req.user._id });
+        await track.save();
+        res.send(track);
+    } catch (error) {
+        res.status(422).send({ error: error.message });
+    };
+
+});
+
 module.exports = router;
